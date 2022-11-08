71f366f53713599ce186a25a70c9a70280d2229a
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-08 07:20:38 -0300

    Bit of classes

--- classes.ts
+++ classes.ts
@@ -1,9 +1,9 @@
 //* no constuctors
 class Employee {
-    id!: number;
-    name!: string;
-    address!: string;
-    email!: string;
+  id!: number;
+  name!: string;
+  address!: string;
+  email!: string;
 }
 
 let emp1 = new Employee();
@@ -13,28 +13,68 @@ emp1.name = "Eduardo";
 emp1.address = "45, Rodeo dr.";
 emp1.email = "edu@edu.com.br";
 
-console.log(emp1)
+console.log(emp1);
 
 class Staff {
-    id!: number;
-    name!: string;
-    address!: string;
-    email!: string;
+  protected id!: number;
+  name!: string;
+  address!: string;
+  email!: string;
+  private idNumber: number;
+  #isOnline: boolean; // another way to maka variables private
 
-    constructor(
-        id: number,
-        name: string,
-        address: string,
-        email:string
-    ) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.email = email;
-    }
+  constructor(
+    id: number,
+    name: string,
+    address: string,
+    email: string,
+    idNumber: number,
+    isOnline: boolean = false
+  ) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.idNumber = idNumber;
+    this.#isOnline = isOnline;
+  }
+
+  getNamewithAddress(): string {
+    return `${this.name.toUpperCase()} at ${this.address}`;
+  }
+
+  static showPay(): number {
+    // can only be accessed by Class name, not obect
+    return 123330;
+  }
 }
 
-let staffMember1 = new Staff(2, "Eduardo", "NYC", "edu@edu.com.br");
+class Manager extends Staff {
+  constructor(
+    id: number,
+    name: string,
+    address: string,
+    email: string,
+    idNumber: number,
+    isOnline: boolean = false
+  ) {
+    super(id, name, address, email, idNumber, isOnline);
+  }
+
+  getNamewithAddress(): string {
+    return `${this.name} is a manager at ${this.address}`;
+  }
+}
+
+let staffMember1 = new Staff(2, "Eduardo", "NYC", "edu@edu.com.br", 12435781);
+
+console.log(Staff.showPay());
 
 console.log(staffMember1);
-   
+
+let address = staffMember1.getNamewithAddress();
+console.log(address);
+
+let manager1 = new Manager(45, "Kevin", "BMW", "kevinH@bmw.com", 123789456);
+
+console.log(manager1.getNamewithAddress());
--- dist/classes.js
+++ dist/classes.js
@@ -1,4 +1,11 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Staff_isOnline;
 class Employee {
 }
 let emp1 = new Employee();
@@ -8,13 +15,36 @@ emp1.address = "45, Rodeo dr.";
 emp1.email = "edu@edu.com.br";
 console.log(emp1);
 class Staff {
-    constructor(id, name, address, email) {
+    constructor(id, name, address, email, idNumber, isOnline = false) {
+        _Staff_isOnline.set(this, void 0);
         this.id = id;
         this.name = name;
         this.address = address;
         this.email = email;
+        this.idNumber = idNumber;
+        __classPrivateFieldSet(this, _Staff_isOnline, isOnline, "f");
+    }
+    getNamewithAddress() {
+        return `${this.name.toUpperCase()} at ${this.address}`;
+    }
+    static showPay() {
+        return 123330;
     }
 }
-let staffMember1 = new Staff(2, "Eduardo", "NYC", "edu@edu.com.br");
+_Staff_isOnline = new WeakMap();
+class Manager extends Staff {
+    constructor(id, name, address, email, idNumber, isOnline = false) {
+        super(id, name, address, email, idNumber, isOnline);
+    }
+    getNamewithAddress() {
+        return `${this.name} is a manager at ${this.address}`;
+    }
+}
+let staffMember1 = new Staff(2, "Eduardo", "NYC", "edu@edu.com.br", 12435781);
+console.log(Staff.showPay());
 console.log(staffMember1);
+let address = staffMember1.getNamewithAddress();
+console.log(address);
+let manager1 = new Manager(45, "Kevin", "BMW", "kevinH@bmw.com", 123789456);
+console.log(manager1.getNamewithAddress());
 //# sourceMappingURL=classes.js.map
